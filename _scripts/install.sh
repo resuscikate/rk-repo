b7d7809a859b1566eca00872b9e934f07aca0c0d
resuscikate <katejuliabeckett@gmail.com>
2017-01-15 21:19:16 +0000

    Test 4

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,10 +2,10 @@
 set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_1fc90f464345_key -iv $encrypted_1fc90f464345_iv -in deploy-key.enc -out deploy-key -d
-rm deploy-key.enc # Don't need it anymore
-chmod 600 deploy-key
-mv deploy-key ~/.ssh/id_rsa
+openssl aes-256-cbc -K $encrypted_1fc90f464345_key -iv $encrypted_1fc90f464345_iv -in kd-deploy.enc -out kd-deploy -d
+rm kd-deploy.enc # Don't need it anymore
+chmod 600 kd-deploy
+mv kd-deploy ~/.ssh/id_rsa
     
 # Install zopfli
 git clone https://github.com/google/zopfli.git
